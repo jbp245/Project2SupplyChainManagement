3ca7327a0fe0c153ae4128055b38b6b4b1c131d4
jeff5489 <jeff5489@gmail.com>
2021-02-10 12:01:20 -0500

    added <Product, Integer> to product repo

--- src/main/java/com/revature/repositories/ProductRepository.java
+++ src/main/java/com/revature/repositories/ProductRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import com.revature.beans.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository{
+public interface ProductRepository extends CrudRepository<Product, Integer>{
 
 }
